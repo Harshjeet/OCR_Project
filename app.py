04cec280aedad6b6a2fb0323bc0cae018bab08a8
harshjeet kumar <harshjeetkumar0@gmail.com>
2025-03-07 00:12:15 +0530

    add the convert button

--- app.py
+++ app.py
@@ -24,64 +24,67 @@ LANGUAGES = {
 st.title("OCR Web application")
 st.write("Upload the image (should be in hindi or english)")
 
-selected_language = st.selectbox("Select the image language: ",options=list(LANGUAGES.keys()))
 
 # creating the file uploader
 upload_image = st.file_uploader("choose an image...", type =["jpg","png", "jpeg"])
 
+selected_language = st.selectbox("Select the image language: ",options=list(LANGUAGES.keys()))
 
-if upload_image is not None:
-    column1, column2 = st.columns([1,1])
-    # open the uploaded image
-    image = Image.open(upload_image)
-    
-    with column1:
-        st.image(image, caption="Uploaded Image", use_container_width=True)
-    
-    #Extract text
-    extracted_text = extract_text_function(image,LANGUAGES[selected_language])
-    
-    with column2:
-        #Display extracted text
-        st.write("Extracted Text: ")
-        st.markdown(
-            f"""
-            <div style="
-                border: 2px solid #4CAF50; 
-                padding: 10px; 
-                border-radius: 10px;
-                background-color: #f4f4f4;
-                max-height: 300px; 
-                overflow-y: auto;">
-                {extracted_text}
-            </div>
-            """,
-            unsafe_allow_html=True,
-        )
+if st.button("convert"):
+    if upload_image is not None:
+        column1, column2 = st.columns([1,1])
+        # open the uploaded image
+        image = Image.open(upload_image)
         
-        #Keyword Search functionalities
-        search_keyword = st.text_input("Enter a keyword to search: ")
+        with column1:
+            st.image(image, caption="Uploaded Image", use_container_width=True)
         
-        if search_keyword:
-            if search_keyword in extracted_text:
-                # Highlight the search keyword
-                highlighted_text = extracted_text.replace(
-                    search_keyword, f'<span style="color:red;">{search_keyword}</span>'
-                )
-                st.write("### search Results: ")
-                st.markdown(
-                    f"""
-                    <div style="
-                        border: 2px solid #FF5733; 
-                        padding: 10px; 
-                        border-radius: 10px;
-                        background-color: #fff3e6;
-                        max-height: 200px; 
-                        overflow-y: auto;">
-                        {highlighted_text}
-                    </div>
-                    """,
-                    unsafe_allow_html=True,
-                )
-            else:
-                st.write(f'no such keyword:{search_keyword}')
+        #Extract text
+        extracted_text = extract_text_function(image,LANGUAGES[selected_language])
+        
+        with column2:
+            #Display extracted text
+            st.write("Extracted Text: ")
+            st.markdown(
+                f"""
+                <div style="
+                    border: 2px solid #4CAF50; 
+                    padding: 10px; 
+                    border-radius: 10px;
+                    background-color: #f4f4f4;
+                    max-height: 300px; 
+                    overflow-y: auto;">
+                    {extracted_text}
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
+            
+            #Keyword Search functionalities
+            search_keyword = st.text_input("Enter a keyword to search: ")
+            
+            if search_keyword:
+                if search_keyword in extracted_text:
+                    # Highlight the search keyword
+                    highlighted_text = extracted_text.replace(
+                        search_keyword, f'<span style="color:red;">{search_keyword}</span>'
+                    )
+                    st.write("### search Results: ")
+                    st.markdown(
+                        f"""
+                        <div style="
+                            border: 2px solid #FF5733; 
+                            padding: 10px; 
+                            border-radius: 10px;
+                            background-color: #fff3e6;
+                            max-height: 200px; 
+                            overflow-y: auto;">
+                            {highlighted_text}
+                        </div>
+                        """,
+                        unsafe_allow_html=True,
+                    )
+                else:
+                    st.write(f'no such keyword:{search_keyword}')
+else:
+    st.warning("load the image first!")
